828a61fb870df5b5b049c73020a2a057652e91af
andrzej <andrzej.b.czajka@gmail.com>
2025-01-15 14:15:27 +0100

    new route file for root

---
+++ code/routes/root.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/hello/")
+def root():
+    return {"message": "Hello World!!!"}
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
+from code.routes import root
 
 app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Hello World!!!"}
+app.include_router(root.router, prefix="/api")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)

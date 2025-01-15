595b9e2beac47ac62f9820f598b3d57db23bf0ee
andrzej <andrzej.b.czajka@gmail.com>
2025-01-15 15:58:43 +0100

    new database file for db connection, added generation of uuid

--- code/routes/raws.py
+++ code/routes/raws.py
@@ -1,7 +1,9 @@
+import uuid
 from fastapi import APIRouter
 
 router = APIRouter()
 
 @router.get("/raws")
 def get_raws():
-    return [{"id":"66666666666666666666", "name": "buckwheat_groats", "kcal/100g": 347, "fat": 3.7, "saturated": 0.6, "carbohydrates": 63, "simple_sugars": 0.7, "fiber": 5.9, "proteins": 14, "salt": 0.1, "cvi": 2.17}, ]
+    idRaw = uuid.uuid4().hex
+    return [{"id":idRaw, "name": "buckwheat_groats", "kcal/100g": 347, "fat": 3.7, "saturated": 0.6, "carbohydrates": 63, "simple_sugars": 0.7, "fiber": 5.9, "proteins": 14, "salt": 0.1, "cvi": 2.17}, ]

1df54b7d3668444b1c6657ab10f7b93368a4ed3e
andrzej <andrzej.b.czajka@gmail.com>
2025-01-16 18:06:39 +0100

    added put and delete methods for raws

--- code/repository/raws.py
+++ code/repository/raws.py
@@ -10,19 +10,59 @@ from code.http.models import RawsResponseModel, RawsModel
 def get_raws(db: Session) -> List[Type[RawsResponseModel]]:
     return db.query(Raws).all()
 
+def get_raw(raw_id: str, db: Session) -> Type[RawsResponseModel]:
+    return search_raw_by_id(raw_id, db)
+
 def create_raw(body: RawsModel, db: Session) -> RawsResponseModel:
-    raw = search_raw(body, db)
+    raw = search_raw_by_name(body.name, db)
     if raw is None:
         id_raw = uuid.uuid4().hex
-        raw = Raws(id=id_raw, name=body.name, kcal_100g=body.kcal_100g, fat=body.fat, saturated_fat=body.saturated_fat, carbohydrates=body.carbohydrates, simple_sugars=body.simple_sugars, fiber=body.fiber, proteins=body.proteins, salt=body.salt, cvi=body.cvi)
+        raw = Raws(id=id_raw, name=body.name, kcal_100g=body.kcal_100g, fat=body.fat, saturated_fat=body.saturated_fat,
+                   carbohydrates=body.carbohydrates, simple_sugars=body.simple_sugars, fiber=body.fiber,
+                   proteins=body.proteins, salt=body.salt, cvi=body.cvi)
         db.add(raw)
         db.commit()
         db.refresh(raw)
     return raw
 
-def search_raw(body: RawsModel, db: Session) -> Type[Raws] | None:
+def update_raw(raw_id: str, body: RawsModel, db: Session) -> Type[RawsResponseModel] | int:
+    raw = search_raw_by_id(raw_id, db)
+    if raw is not None:
+        if raw.name != body.name:
+            return -2
+        raw.kcal_100g=body.kcal_100g
+        raw.fat=body.fat,
+        raw.saturated_fat=body.saturated_fat
+        raw.carbohydrates=body.carbohydrates
+        raw.simple_sugars=body.simple_sugars
+        raw.fiber=body.fiber
+        raw.proteins=body.proteins
+        raw.salt=body.salt
+        raw.cvi=body.cvi
+        db.commit()
+        return raw
+    return -1
+
+def remove_raw(raw_id: str, body: RawsModel, db: Session) -> int | None:
+    existing_raw = search_raw_by_id(raw_id, db)
+    if existing_raw is not None:
+        if existing_raw.name != body.name:
+            return -2
+        db.delete(existing_raw)
+        db.commit()
+        return 0
+    return -1
+
+def search_raw_by_name(raw_name: str, db: Session) -> Type[Raws] | None:
     existing_raws = db.query(Raws).all()
     for raw in existing_raws:
-        if body.name == raw.name:
+        if raw_name == raw.name:
+            return raw
+    return None
+
+def search_raw_by_id(raw_id: str, db: Session):
+    existing_raws = db.query(Raws).all()
+    for raw in existing_raws:
+        if raw_id == raw.id:
             return raw
     return None
--- code/routes/raws.py
+++ code/routes/raws.py
@@ -17,6 +17,31 @@ def get_raws(db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="raws not found")
     return raws
 
+@router.get("/{raw_id}", response_model=RawsResponseModel)
+def get_raw(raw_id: str, db: Session = Depends(get_db)):
+    raw = rep_raws.get_raw(raw_id, db)
+    if raw is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="raw not found")
+    return raw
+
 @router.post("/", response_model=RawsResponseModel, status_code=status.HTTP_201_CREATED)
 def create_raw(body: RawsModel, db: Session = Depends(get_db)):
     return rep_raws.create_raw(body, db)
+
+@router.put("/{raw_id}", response_model=RawsResponseModel)
+def put_raw(raw_id: str, body: RawsModel, db: Session = Depends(get_db)):
+    raw = rep_raws.update_raw(raw_id, body, db)
+    if raw == -1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="raw not found")
+    if raw == -2:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="raw name incorrect")
+    return raw
+
+@router.delete("/{raw_id}", response_model=List[RawsResponseModel])
+def delete_raw(raw_id: str, body: RawsModel, db: Session = Depends(get_db)):
+    raw = rep_raws.remove_raw(raw_id, body, db)
+    if raw == -1:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="raw not found")
+    if raw == -2:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="raw name incorrect")
+    return []

f1283f9eae9569e64b123f68672dc0426370a757
andrzej <andrzej.b.czajka@gmail.com>
2025-02-04 22:53:24 +0100

    Added update_dish test

--- tests/dishes_test.py
+++ tests/dishes_test.py
@@ -1,14 +1,17 @@
 from ..code.database.models import Dishes
 from ..code.http.models import DishesModel
-from ..code.repository.dishes import get_dishes, create_dish, get_dish, remove_dish
+from ..code.repository.dishes import get_dishes, create_dish, get_dish, remove_dish, update_dish
 from .constants import db
 
-global dish_id
+dish_id = 0
+dishes_amount = 0
 
 def test_if_get_dishes_returns_list():
     # Given
     # When
     dishes = get_dishes(db=db)
+    global dishes_amount
+    dishes_amount = len(dishes)
     # Then
     assert isinstance(dishes, list)
 
@@ -35,6 +38,37 @@ def test_if_get_dish_returns_newly_added_dish_id():
     # Then
     assert dish.id == dish_id
 
+def test_if_get_dishes_returns_one_dish_more_than_previously():
+    # Given
+    # When
+    dishes = get_dishes(db=db)
+    # Then
+    assert len(dishes) == dishes_amount + 1
+
+def test_if_update_dish_returns_dishes_response_model():
+    # Given
+    dish = DishesModel(name="aaa", kcal_100g=1)
+
+    # When
+    new_dish = update_dish(dish_id, dish, db)
+
+    # Then
+    assert isinstance(new_dish, Dishes)
+
+def test_if_get_dishes_returns_same_amount_of_dishes_as_before_update():
+    # Given
+    # When
+    dishes = get_dishes(db=db)
+    # Then
+    assert len(dishes) == dishes_amount + 1
+
+def test_if_dish_has_been_updated_correctly():
+    # Given
+    # When
+    dish = get_dish(dish_id, db=db)
+    # Then
+    assert dish.kcal_100g == 1
+
 def test_if_remove_dish_returns_0():
     # Given
     dish = DishesModel(name="aaa")
@@ -44,3 +78,11 @@ def test_if_remove_dish_returns_0():
 
     # Then
     assert return_value == 0
+
+def test_if_dish_has_been_truly_removed():
+    # Given
+    # When
+    dishes = get_dishes(db=db)
+    # Then
+    assert len(dishes) == dishes_amount
+
